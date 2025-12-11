233ca727d0b8cf54a19c3f0d2d200c29f95affa1
tenkus47 <tenkus47@gmail.com>
2025-12-11 13:25:54 +0530

    chore: update Prisma schema configuration
    - Added DATABASE_URL environment variable to the schema.prisma file for database connection.
    - Removed the unnecessary prisma.config.js file, streamlining the backend setup.

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,10 +1,7 @@
-const { PrismaClient } = require("@prisma/client");
-const { Pool } = require("pg");
-const { PrismaPg } = require("@prisma/adapter-pg");
-require("dotenv").config();
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 const prisma = new PrismaClient({ adapter });
-
-module.exports = { prisma };
